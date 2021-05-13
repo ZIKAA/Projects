cfcbd66ed7fcab022c06db364000307440fb5c91
ashinnaw <ahmedshinnawi@gmail.com>
2021-05-13 12:16:48 +0200

    [feat] implementing the client/server nodes code for weather journal app.

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,9 +1,13 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-
+const portNum=3000;
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app= express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+app.listen(portNum,()=>{
+	console.log('server is up and running on port number '+portNum);
+});
+// Send data to client
+app.get('/getData', (req,res)=>{
+	res.send(projectData);
+});
+
+// Save data
+app.post('/saveData', (req,res)=>{
+	projectData = req.body;
+	res.end();
+});
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,70 @@
-/* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const apiKey='85259c14ab8dc2072a2581428352c97e';
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+//get the button element from web page
+const button = document.getElementById('generate');
+
+// Event listener to add function to existing HTML DOM element
+button.addEventListener('click',  async function eventHandler() {
+	try{
+		getWhetherInfo()
+		.then((input) => {
+			return serverHandler(input)		
+		})
+		.then((input)=>{
+			updateUiData(input)
+		})
+	}
+	catch(error)
+	{
+		console.log(error);
+	}
+});
+
+
+/* Function called by event listener */
+
+/* Function to GET Web API Data*/
+async function getWhetherInfo(){
+	const zipCode= document.getElementById('zip').value;
+	const feels= document.getElementById('feelings').value;
+	 const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=metric`);
+	const apiData = await res.json();
+	// Create a new date instance dynamically with JS
+	let d = new Date();
+	let newDate = +d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
+	const ret =  {
+		date: newDate,
+		feelings: feels,
+		temp: apiData.main.temp};
+	return ret;
+}
+
+/* Function to POST data */
+/* Function to GET Project Data */
+async function serverHandler(input){
+	await fetch('/saveData', {
+		method: 'POST',
+		credentials: 'same-origin', 
+		headers: {
+		  'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(input)  
+    });
+	const res= await fetch('/getData', {
+		method: 'GET',
+		credentials: 'same-origin',
+		headers:{
+			"Content-Type": 'application/json'
+		}
+	});
+	const output = res.json();
+	return output;
+}
+
+function updateUiData(input){
+	 document.getElementById('temp').innerHTML = 'Temperature: ' + input.temp + ' degC';
+	 document.getElementById('date').innerHTML = 'Date: ' + input.date; 
+     document.getElementById('content').innerHTML = 'Feels: ' + input.feelings;
+	
+}
